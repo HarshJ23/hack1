cca412c3bd3ad0659c24de86ee4d5e93d009e1b8
HarshJ23 <hjawajiwar@gmail.com>
2024-01-28 04:54:55 +0530

    feature-info added

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from llama_index import SimpleDirectoryReader
 st.set_page_config(page_title="Handout bot tool", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
 st.title("Find info from handouts quickly!")
-# st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
+st.info("Features include find probable PS2 stations according to your CGPA , Get details of the professor according to your research interests , Get info fron handouts quickly , analyse previous year question papers.", icon="📃")
          
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
